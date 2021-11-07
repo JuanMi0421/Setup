5e0d3ed4016068809c387aaaed1501c2a4f2738c
JuanMi0421 <87953932+JuanMi0421@users.noreply.github.com>
2021-11-07 17:30:32 -0500

    Update starship.bash

--- starship.bash
+++ starship.bash
@@ -2,7 +2,9 @@
 echo "Please enter password"
 sudo echo "Thanks!"
 
-#-----------------------------------Install Nerd Font--------------------------------#
+###################################### Starship ######################################
+
+#--------------------------------------Nerd Font-------------------------------------#
 cd /usr/share/fonts
 
 sudo mkdir nerd_fonts
@@ -12,13 +14,15 @@ sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave
 
 sudo unzip Agave.zip 
 sudo rm Agave.zip
-#------------------------------------------------------------------------------------#
+#--------------------------------------Nerd Font-------------------------------------#
 
 #-----------------------------------Install Starship---------------------------------#
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
 echo "eval "$(starship init bash)"" >> ~/.bashrc
+#-----------------------------------Install Starship---------------------------------#
 
+#-------------------------------------Config file------------------------------------#
 mkdir -p ~/.config && touch ~/.config/starship.toml
 
 echo '# Inserts a blank line between shell prompts
@@ -31,4 +35,38 @@ success_symbol = "[➜](bold green)"     # The "success_symbol" segment is being
 # Disable the package module, hiding it from the prompt completely
 [package]
 disabled = true' >> ~/.config/starship.toml
-#------------------------------------------------------------------------------------#
+#-------------------------------------Config file------------------------------------#
+
+###################################### Starship ######################################
+
+
+
+#################################### Dracula Theme ###################################
+
+#------------------------------------Gnome Terminal----------------------------------#
+sudo apt-get install dconf-cli
+
+git clone https://github.com/dracula/gnome-terminal
+
+cd gnome-terminal
+./install.sh
+#------------------------------------Gnome Terminal----------------------------------#
+
+#-----------------------------------------GTK----------------------------------------#
+cd /usr/share/themes
+sudo wget https://github.com/dracula/gtk/archive/master.zip
+
+sudo unzip master.zip
+
+sudo gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+sudo gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+
+cd /usr/share/icons
+sudo wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
+
+sudo unzip Dracula.zip
+
+sudo gsettings set org.gnome.desktop.interface icon-theme "Dracula"
+#-----------------------------------------GTK----------------------------------------#
+
+#################################### Dracula Theme ###################################
